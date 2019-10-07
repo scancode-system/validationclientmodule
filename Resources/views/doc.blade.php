0b101ab21cdf75f0fe6a3b8d93d4842e6fecc164
Your Name <you@example.com>
2019-10-07 01:30:10 -0300

    .

---
+++ Resources/views/doc.blade.php
@@ -0,0 +1,168 @@
+<div class="alert alert-info" role="alert">
+    <strong>Dados obrigatórios:</strong> cpf_cnpj, razao_social
+</div>
+<table class="table table-bordered">
+  <thead>
+    <tr>
+        <th scope="col">Campo</th>
+        <th scope="col">Tipo</th>
+        <th scope="col">Min</th>
+        <th scope="col">Max</th>
+    </tr> 
+</thead>
+<tbody>
+    <tr>
+        <td scope="row">
+            <span data-toggle="tooltip" data-placement="top" title="Número identificador do cliente, código do cliente">id_cliente</span>
+        </td>
+        <td>Inteiro</td>
+        <td>1</td>
+        <td>N/A</td>
+    </tr>
+    <tr>
+        <td scope="row">
+            <span data-toggle="tooltip" data-placement="top" title="Nome fantasia da empresa">nome_fantasia</span>
+        </td>
+        <td>Texto</td>
+        <td>N/A</td>
+        <td>255</td>
+    </tr>
+    <tr>
+        <td scope="row">
+            <span data-toggle="tooltip" data-placement="top" title="Razão social da empresa"><strong>razao_social</strong></span>
+        </td>
+        <td>Texto</td>
+        <td>N/A</td>
+        <td>255</td>
+    </tr>
+    <tr>
+        <td scope="row">
+            <span data-toggle="tooltip" data-placement="top" title="O CPF ou CNPJ do cliente"><strong>cpf_cnpj</strong></span>
+        </td>
+        <td>Texto</td>
+        <td>N/A</td>
+        <td>18</td>
+    </tr>
+    <tr>
+        <td scope="row">
+            <span data-toggle="tooltip" data-placement="top" title="Inscrição Estadual da empresa">inscricao_estadual</span>
+        </td>
+        <td>Texto</td>
+        <td>N/A</td>
+        <td>100</td>
+    </tr>
+    <tr>
+        <td scope="row">
+            <span data-toggle="tooltip" data-placement="top" title="Nome o contato">nome_contato</span>
+        </td>
+        <td>Texto</td>
+        <td>N/A</td>
+        <td>255</td>
+    </tr>
+    <tr>
+        <td scope="row">
+            <span data-toggle="tooltip" data-placement="top" title="Nome do comprador">nome_comprador</span>
+        </td>
+        <td>Texto</td>
+        <td>N/A</td>
+        <td>30</td>
+    </tr>
+    <tr>
+        <td scope="row">
+            <span data-toggle="tooltip" data-placement="top" title="Email da empresa">email</span>
+        </td>
+        <td>Texto</td>
+        <td>N/A</td>
+        <td>60</td>
+    </tr>
+    <tr>
+        <td scope="row">
+            <span data-toggle="tooltip" data-placement="top" title="Número do telefone prinncipal">tel_1</span>
+        </td>
+        <td>Texto</td>
+        <td>N/A</td>
+        <td>15</td>
+    </tr>
+    <tr>
+        <td scope="row">
+            <span data-toggle="tooltip" data-placement="top" title="Número do telefone secundário">tel_2</span>
+        </td>
+        <td>Texto</td>
+        <td>N/A</td>
+        <td>15</td>
+    </tr>
+    <tr>
+        <td scope="row">
+            <span data-toggle="tooltip" data-placement="top" title="Observação a respeito do cliente">observacao</span>
+        </td>
+        <td>Texto</td>
+        <td>N/A</td>
+        <td>N/A</td>
+    </tr>
+    <tr>
+        <td scope="row">
+            <span data-toggle="tooltip" data-placement="top" title="Cliente compra pelo preço varejo">varejo</span>
+        </td>
+        <td>Inteiro</td>
+        <td>0</td>
+        <td>1</td>
+    </tr>
+    <tr>
+        <td scope="row">
+            <span data-toggle="tooltip" data-placement="top" title="Endereço: rua e número">logradouro</span>
+        </td>
+        <td>Texto</td>
+        <td>N/A</td>
+        <td>255</td>
+    </tr>
+    <tr>
+        <td scope="row">
+            <span data-toggle="tooltip" data-placement="top" title="Endereço: nome do bairro">bairro</span>
+        </td>
+        <td>Texto</td>
+        <td>N/A</td>
+        <td>255</td>
+    </tr>
+    <tr>
+        <td scope="row">
+            <span data-toggle="tooltip" data-placement="top" title="Endereço: nome da cidade">cidade</span>
+        </td>
+        <td>Texto</td>
+        <td>N/A</td>
+        <td>255</td>
+    </tr>
+    <tr>
+        <td scope="row">
+            <span data-toggle="tooltip" data-placement="top" title="Endereço: nome do estado abreviado">uf</span>
+        </td>
+        <td>Texto</td>
+        <td>N/A</td>
+        <td>2</td>
+    </tr>
+    <tr>
+        <td scope="row">
+            <span data-toggle="tooltip" data-placement="top" title="Endereço: número do cep">cep</span>
+        </td>
+        <td>Testo</td>
+        <td>N/A</td>
+        <td>10</td>
+    </tr>
+    <tr>
+        <td scope="row">
+            <span data-toggle="tooltip" data-placement="top" title="Classificação do cliente, exemplo (diamante, ouro, prata, bronze...)">cliente_status</span>
+        </td>
+        <td>Texto</td>
+        <td>N/A</td>
+        <td>255</td>
+    </tr>
+    <tr>
+        <td scope="row">
+            <span data-toggle="tooltip" data-placement="top" title="Número identificador do pagamento, código do pagamento">transportadora</span>
+        </td>
+        <td>Texto</td>
+        <td>N/A</td>
+        <td>255</td>
+    </tr>
+
+</tbody>
+</table>
