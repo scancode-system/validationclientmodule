6490181f909b0b77b3cd3561fc73b82206ed26a2
Your Name <you@example.com>
2019-12-06 00:33:32 -0300

    .

--- Validator/ClientsValidator.php
+++ Validator/ClientsValidator.php
@@ -9,6 +9,31 @@ use Modules\Portal\Rules\NotInCustomRule;
 class ClientsValidator extends ValidatorImport
 {
 
+	protected $required = ['id', 'corporate_name', 'fantasy_name', 'cpf_cnpj', 'buyer', 'email', 'phone', 'shipping_company_id', 'street', 'number', 'apartment', 'neighborhood', 'city', 'st', 'postcode'];
+
+
+	public function rule($data){
+		return [
+			'id' =>	['integer', 'min:1', new NotInCustomRule($this->chunkColumn('id_cliente', 0, $this->row_index-2), 'Duplicado')],
+			'corporate_name' =>	['filled', 'string', 'max:255',new NotInCustomRule($this->chunkColumn('corporate_name', 0, $this->row_index-2), 'Duplicado')]
+			'fantasy_name' => 'string|max:255',
+			'cpf_cnpj' => ['string', 'max:255', new NotInCustomRule($this->chunkColumn('cpf_cnpj', 0, $this->row_index-2), 'Duplicado')],
+			'buyer' => 'string|max:255',
+			'email' => 'string|max:255',
+			'phone' => 'string|max:255',
+			'shipping_company_id' => 'integer|min:1',
+			'street' =>	'string|max:255',
+			'number' =>	'integer|min:0',
+			'apartment' => 'string|max:255',
+			'neighborhood' => 'string|max:255',
+			'city' => 'string|max:255',
+			'st' =>	'string|max:2',
+			'postcode' => 'string|max:8'
+		];
+	}
+
+
+/*
 	protected $required = ['cpf_cnpj', 'razao_social'];
 
 	public function rule($data){
@@ -44,6 +69,6 @@ class ClientsValidator extends ValidatorImport
 
 	public function messages(){
 		return  [];
-	}
+	}*/
 
 }
